910b604e73ffbb2c0e2dd3c3dfb2b7e09a86afe6
Rahul <rahulhsv3@gmail.com>
2021-09-13 11:12:32 +0530

    new class added

--- myfirstrestservice/src/main/java/com/brillio/myfirstrestservice/Class2.java
+++ myfirstrestservice/src/main/java/com/brillio/myfirstrestservice/Class2.java
@@ -1,2 +1,7 @@
-package com.brillio.myfirstrestservice;public class Class2 {
+package com.brillio.myfirstrestservice;
+
+public class Class2 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
 }
